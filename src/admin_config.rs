efb537846c73d1268d75356a0bf3c11991b541db
Jonas Pfeifer <jonaspfeifer@drei.at>
2024-04-04 19:47:23 +0200

    added generated hash and function to compare hash

--- src/admin_config.rs
+++ src/admin_config.rs
@@ -1,27 +1,28 @@
-use std::fs;
 use anyhow::Result;
 use serde::{Deserialize, Serialize};
+use std::fs;
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct AdminConfig {
-    credentials: Credentials
-}
-
-#[allow(unused)]
-impl AdminConfig {
-    pub fn credentials(&self) -> &Credentials {
-        &self.credentials
-    }
+    pub credentials: Credentials,
 }
 
 impl AdminConfig {
     pub fn read() -> Result<AdminConfig> {
         let config_string = fs::read_to_string("./data/config/admin.toml")?;
-        Ok(toml::from_str(&config_string)?)
+        let mut config: AdminConfig = toml::from_str(&config_string)?;
+        config.credentials.hash = sha256::digest(config.credentials.password.clone());
+        Ok(config)
+    }
+
+    pub fn check_admin_hash(&self, password_hash: String) -> bool {
+        password_hash.eq(&self.credentials.hash)
     }
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct Credentials {
-    password: String
+    password: String,
+    #[serde(skip_deserializing)]
+    hash: String,
 }

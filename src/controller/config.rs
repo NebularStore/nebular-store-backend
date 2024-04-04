c7945a1af4596d8dab7a5820efdbf4af90a36955
Jonas Pfeifer <jonaspfeifer@drei.at>
2024-04-04 20:23:15 +0200

    added route for changing config

---
+++ src/controller/config.rs
@@ -0,0 +1,96 @@
+use crate::state::SharedState;
+use axum::extract::Query;
+use axum::http::StatusCode;
+use axum::response::IntoResponse;
+use axum::routing::{get, post};
+use axum::{Extension, Json, Router};
+use serde::Deserialize;
+
+pub fn config_router() -> Router {
+    Router::new()
+        .route("/general", get(general_config))
+        .route("/admin", get(admin_config))
+        .route("/check_admin", get(check_admin_hash))
+        .route("/change", post(change_config))
+}
+
+async fn general_config(Extension(state): Extension<SharedState>) -> impl IntoResponse {
+    Json(state.read().await.general_config().clone())
+}
+
+async fn admin_config(Extension(state): Extension<SharedState>) -> impl IntoResponse {
+    Json(state.read().await.admin_config().clone())
+}
+
+#[derive(Deserialize)]
+pub struct AdminCheckParams {
+    admin_hash: String,
+}
+async fn check_admin_hash(
+    Extension(state): Extension<SharedState>,
+    admin_hash: Query<AdminCheckParams>,
+) -> impl IntoResponse {
+    state
+        .read()
+        .await
+        .admin_config()
+        .check_admin_hash(admin_hash.0.admin_hash)
+        .to_string()
+}
+
+#[derive(Deserialize)]
+pub struct ChangeConfigPayload {
+    path: String,
+    value: String,
+}
+async fn change_config(
+    Extension(state): Extension<SharedState>,
+    Json(payload): Json<ChangeConfigPayload>,
+) -> impl IntoResponse {
+    let mut path = payload.path.split('.');
+    let category = match path.next() {
+        None => return (StatusCode::BAD_REQUEST, "No category provided"),
+        Some(category) => category.to_lowercase(),
+    };
+    let part = match path.next() {
+        None => return (StatusCode::BAD_REQUEST, "No part provided"),
+        Some(part) => part.to_lowercase(),
+    };
+    let param = match path.next() {
+        None => return (StatusCode::BAD_REQUEST, "No param provided"),
+        Some(param) => param.to_lowercase(),
+    };
+
+    let worked = match category.as_str() {
+        "admin" => match part.as_str() {
+            "credentials" => match param.as_str() {
+                "password" => true,
+                _ => false,
+            },
+            _ => false,
+        },
+        "general" => match part.as_str() {
+            "server" => match param.as_str() {
+                "port" => true,
+                _ => false,
+            }
+            "theme" => match param.as_str() {
+                "icon_path" => true,
+                "company_name" => true,
+                _ => false,
+            }
+            "logging" => match param.as_str() {
+                "max_level" => true,
+                _ => false,
+            }
+            _ => false
+        },
+        _ => false,
+    };
+
+    if worked {
+        (StatusCode::OK, "")
+    } else {
+        (StatusCode::BAD_REQUEST, "Unknown config")
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,17 @@
 use crate::general_config::TracingLevel;
-use crate::state::{init_state, SharedState};
-use axum::response::IntoResponse;
-use axum::routing::get;
-use axum::{Extension, Json, Router};
+use crate::state::{init_state};
+use axum::{Router};
 use std::net::SocketAddr;
-use axum::extract::Query;
-use serde::Deserialize;
 use tower::ServiceBuilder;
 use tower_http::add_extension::AddExtensionLayer;
 use tower_http::trace::TraceLayer;
 use tracing::info;
+use crate::controller::config::config_router;
 
 mod general_config;
 mod state;
 mod admin_config;
+mod controller;
 
 #[tokio::main]
 async fn main() {
@@ -29,10 +27,9 @@ async fn main() {
         .unwrap_or(TracingLevel::Warn)
         .level();
     tracing_subscriber::fmt().with_max_level(level).init();
+
     let app = Router::new()
-        .route("/general_config", get(general_config))
-        .route("/admin_config", get(admin_config))
-        .route("/check_admin_hash", get(check_admin_hash))
+        .nest("/config", config_router())
         .layer(
             ServiceBuilder::new()
                 .layer(AddExtensionLayer::new(state.clone()))
@@ -47,20 +44,4 @@ async fn main() {
         .expect("Failed to create tcp listener");
     info!("Starting server on port {}", port);
     axum::serve(listener, app).await.expect("Failed to serve")
-}
-
-async fn general_config(Extension(state): Extension<SharedState>) -> impl IntoResponse {
-    Json(state.read().await.general_config().clone())
-}
-
-async fn admin_config(Extension(state): Extension<SharedState>) -> impl IntoResponse {
-    Json(state.read().await.admin_config().clone())
-}
-
-#[derive(Deserialize)]
-pub struct AdminCheckParams {
-    admin_hash: String
-}
-async fn check_admin_hash(Extension(state): Extension<SharedState>, admin_hash: Query<AdminCheckParams>) -> impl IntoResponse {
-    state.read().await.admin_config().check_admin_hash(admin_hash.0.admin_hash).to_string()
 }

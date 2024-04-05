2cb163705bd4c38bb0c977662ea16ca2e2d937bc
Jonas Pfeifer <jonaspfeifer@drei.at>
2024-04-05 08:08:34 +0200

    serve repository files

--- src/controller/files.rs
+++ src/controller/files.rs
@@ -1,5 +1,7 @@
 use axum::Router;
+use tower_http::services::ServeDir;
 
 pub fn files_router() -> Router {
     Router::new()
+        .nest_service("/repository", ServeDir::new("data/repository"))
 }
